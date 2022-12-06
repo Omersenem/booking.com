581e2f3f699285a83aca9d5fc6aac623ead0cd29
Arge <arge@Arges-iMac.local>
2022-12-06 23:34:23 +0300

    parcel_ornek

---
+++ dist/index.72be8890.js
@@ -0,0 +1,6 @@
+$(".Header-switch").on("click", function() {
+    if ($("Header").is(".menu-show")) $(".Header").removeClass("menu-show");
+    else $(".Header").addClass("menu-show");
+});
+
+//# sourceMappingURL=index.72be8890.js.map
